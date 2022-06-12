8a0401303ada364dcab4f3659ecc432028e9ac35
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-12 12:52:33 -0400

    Use description from package.json

--- src/cli.ts
+++ src/cli.ts
@@ -2,14 +2,14 @@ import * as sade from "sade";
 import handler from "./handler";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires -- Needed in order to not copy over package.json and make new src directory inside of dist
-const { version } = require("../package.json");
+const { version, description } = require("../package.json");
 
 const cli = sade(
   "github-copy-secrets <secretsDirectory> <owner> <repository>",
   true
 )
   .version(version)
-  .describe("Copy secrets from a directory into a GitHub repository.")
+  .describe(description)
   .option("-t, --token", "GitHub token")
   .option("-l, --list", "List secrets that will be copied.")
   .action(handler);
